3132d8a0fe685e1193f7b1db8c113420723c02d8
MatalasG <matalasgiorgos@gmail.com>
2024-05-09 02:13:00 +0300

    players command commit

---
+++ commands/utility/players.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+module.exports = {
+    data: {
+        name: 'players',
+        description: 'Check the list of connected players.',
+    },
+    async execute(interaction) {
+        try {
+            const data = fs.readFileSync('serverLog.json', 'utf8');
+            const serverStatus = JSON.parse(data);
+            const players = serverStatus.players.join(', ');
+            await interaction.reply(`Currently connected players: ${players}`);
+        } catch (error) {
+            console.error('Error reading server log data:', error);
+            await interaction.reply('Error reading server log data.');
+        }
+    },
+};
