430a9e008802d038ff2a8c19fcdb188588312bd0
MatalasG <matalasgiorgos@gmail.com>
2024-05-21 11:12:12 +0300

    players.js get/ post connected players names

--- commands/utility/khrom.js
+++ commands/utility/khrom.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('khrom')
-		.setDescription('pay your respects weakling'),
+		.setDescription('Pay your respects weakling'),
 	async execute(interaction) {
 		await interaction.reply(
             'General Khrom'
